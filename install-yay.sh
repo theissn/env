7aeb1cdaf58f8b2cfefe174491cde4e5cda5475e
theissn <theissnielsen@gmail.com>
2025-07-12 13:18:27 +0100

    updates

---
+++ install-yay.sh
@@ -0,0 +1,24 @@
+yay --needed -S \
+	chromium \
+	docker docker-compose \
+	php php-sqlite composer \
+	git lazygit \
+	go \
+	hyprland hyprlock hyprshot brightnessctl mako waybar wofi \
+	xdg-desktop-portal-hyprland hyprpolkitagent \
+	bluez bluez-utils blueman \
+	bash-completion \
+	btop \
+	mise \
+	openssh \
+	pipewire wireplumber pavucontrol \
+	ttf-font-awesome noto-fonts-emoji \
+	tmux \
+	zathura zathura-pdf-poppler \
+	neovim vim \
+	tlp tlp-rdw \
+	tree \
+	ripgrep \
+	ghostty
+
+	
