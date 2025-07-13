ae61a0d740361ba07bf81ac98d876351ee01d8a1
theissn <theissnielsen@gmail.com>
2025-07-13 12:45:13 +0100

    updates

--- copy.sh
+++ copy.sh
@@ -1,4 +1,7 @@
 
 cp -r ~/.config/hypr ~/.config/bash ~/.config/tmux ~/.config/waybar  ~/.config/wofi ~/.config/mpv ./config
+
 cp -r ~/.config/nvim/init.lua ./config/nvim
+
+cp -r ~/.local/bin ./local
 
