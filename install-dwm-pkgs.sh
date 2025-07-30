c2bcfeae1dee89be7d310f0a388ed52c2e697b49
theissn <theissnielsen@gmail.com>
2025-07-30 08:59:54 +0100

    packages

--- install-dwm-pkgs.sh
+++ install-dwm-pkgs.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --needed \
-	vim base-devel git libx11 libxft xorg-server xorg-xinit ttf-font-awesome ttf-dejavu firefox chromium bluez bluez-utils neovim btop \
+	vim base-devel git libx11 libxft xorg-server xorg-xinit ttf-font-awesome ttf-dejavu firefox chromium bluez bluez-utils neovim htop \
 	ripgrep fzf tmux openssh xclip dunst libnotify gnome-keyring newsboat pavucontrol \
 	yt-dlp mpv ffmpeg zathura zathura-pdf-mupdf tesseract-data-eng slock man-db gnome-themes-extra \
 	wireguard-tools openresolv mise \
@@ -9,4 +9,4 @@ sudo pacman -S --needed \
 
 systemctl --user start ssh-agent.service
 sudo systemctl enable --now bluetooth.service
-sudo systemctl enable --now rtkit-daemon
+sudo systemctl enable --now rtkit-daemon.service
