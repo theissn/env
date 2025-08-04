338acc06d476185c6917fbaec9b82a28dd1b0489
theissn <theissnielsen@gmail.com>
2025-08-04 07:57:53 +0100

    neovim conf

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,85 +1,175 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.termguicolors = true
-vim.o.wrap = false
-vim.o.tabstop = 4
-vim.o.swapfile = false
+-- init.lua
+-- Heavily simplified Neovim config based on kickstart.nvim
+
 vim.g.mapleader = " "
-vim.o.winborder = "rounded"
--- vim.o.clipboard = "unnamedplus"
-vim.opt.rtp:append(vim.fn.stdpath('config') .. '/lua')
+vim.g.maplocalleader = " "
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+-- Basic options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+-- vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+-- Basic keymaps
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
-vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
-	{ src = "https://github.com/kdheepak/lazygit.nvim" },
+-- Install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+	{ "tpope/vim-sleuth" }, -- Detect tabstop/shiftwidth
+
+	-- Theme: vague.nvim
+	{
+		"vague2k/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vague").setup({})
+			vim.cmd.colorscheme("vague")
+		end,
+	},
+
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
+		},
+		config = function()
+			require("telescope").setup({})
+			require("telescope").load_extension("fzf")
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search Grep" })
+			vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Search Buffers" })
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
+			vim.keymap.set("n", "<leader>sq", builtin.diagnostics)
+		end,
+	},
+
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "lua", "vimdoc", "bash", "markdown" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+
+	-- LSP + Mason
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-tool-installer").setup({ ensure_installed = { "stylua" } })
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+				},
+			})
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+					map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+					map("gr", require("telescope.builtin").lsp_references, "Goto References")
+					map("K", vim.lsp.buf.hover, "Hover")
+					map("<leader>rn", vim.lsp.buf.rename, "Rename")
+					map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+				end,
+			})
+		end,
+	},
+
+	-- blink.cmp
+	{
+		"Saghen/blink.cmp",
+		lazy = false,
+		dependencies = "rafamadriz/friendly-snippets",
+		version = "v3.*",
+		opts = {
+			highlight = { use_nvim_cmp_as_default = true },
+			nerd_font_variant = "normal",
+			keymap = { preset = "default" },
+			accept = { auto_brackets = { enabled = true } },
+			trigger = { signature_help = { enabled = true } },
+		},
+	},
+
+	-- oil.nvim
+	{
+		"stevearc/oil.nvim",
+		lazy = false,
+		dependencies = { "echasnovski/mini.icons" },
+		config = function()
+			require("oil").setup()
+			vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+
+	-- conform.nvim
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Add more as needed
+				},
+			})
+			vim.keymap.set("n", "<leader>cf", function()
+				require("conform").format({ lsp_format = "fallback" })
+			end, { desc = "Format buffer" })
+		end,
+	},
 })
-
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
-		end
-	end,
-})
-vim.cmd("set completeopt+=noselect")
-
-require "mini.pick".setup()
-require "nvim-treesitter.configs".setup({
-	ensure_installed = { "typescript", "javascript", "php", "blade" },
-	highlight = { enable = true }
-})
-require "oil".setup()
-
-vim.keymap.set('n', '<leader>df', ":Pick files<CR>")
-vim.keymap.set('n', '<leader>dg', ":Pick grep_live<CR>")
-vim.keymap.set('n', '<leader>db', ":Pick buffers<CR>")
-vim.keymap.set('n', '<leader>dh', ":Pick help<CR>")
-vim.keymap.set('n', '<leader>e', ":Oil<CR>")
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<cr>')
-
-vim.lsp.enable({ "lua_ls", "phpactor" })
-
-require "vague".setup({ transparent = true })
-vim.cmd("colorscheme vague")
-vim.cmd(":hi statusline guibg=NONE")
-
-local gemini_key = os.getenv("GEMINI_KEY")
-require('gemini').setup{api_key=gemini_key}
-
-vim.api.nvim_create_user_command(
-  "Gemini",
-  function()
-    require("gemini").ask_gemini()
-  end,
-  { nargs = 0 }
-)
-
-vim.keymap.set('n', '<leader>g', ':Gemini<CR>')
-
-local openrouter_key = os.getenv("OR_KEY")
-require('openrouter').setup{api_key=openrouter_key}
-
-vim.api.nvim_create_user_command(
-  "OpenRouter",
-  function()
-    require("openrouter").ask_openrouter()
-  end,
-  { nargs = 0 }
-)
-
-vim.keymap.set('n', '<leader>or', ':OpenRouter<CR>')
